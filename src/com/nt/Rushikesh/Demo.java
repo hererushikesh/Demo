027e08398133741418a664d0bf3ef245f6766af1
Rushikesh Here <rushikeshhere@gmail.com>
2024-01-16 09:29:15 +0530

    Adding somthing new

---
+++ src/com/nt/Rushikesh/Demo.java
@@ -0,0 +1,17 @@
+package com.nt.Rushikesh;
+
+public class Demo {
+	public static void main(String[] args) {
+		
+		int a=128,b=128;
+		
+		System.out.println(a==b);
+		
+		int c=100,d=100;
+		System.out.println(c==d);
+		
+		
+		
+	}
+
+}
